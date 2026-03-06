6d16f6d7cd60d839137d3a3c0d7d2f33dbca1ca7
Jerelynm <jmarinm4@miumg.edu.gt>
2026-03-06 09:59:42 -0600

    se realizan modificaciones y agregan nuevas funciones

--- universidad/Models/Alumno/models.py
+++ universidad/Models/Alumno/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
+
 class Alumno(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    ]
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
-    gender = models.CharField(max_length=1)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     birth_date = models.DateField()
-    enrolled_at = models.DateField(auto_now_add=True)
+    enrolled_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
-    class Meta:
-        db_table = 'alumno'
+        return f'{self.first_name} {self.last_name}'
